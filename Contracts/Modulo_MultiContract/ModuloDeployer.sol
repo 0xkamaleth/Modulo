862d3d59fcdd5c538d8b6c4d8fa7ad47c14eff8b
0xkamaleth <0xkamaleth@proton.me>
2022-11-28 21:43:13 +0000

    more

--- Contracts/Modulo_MultiContract/ModuloDeployer.sol
+++ Contracts/Modulo_MultiContract/ModuloDeployer.sol
@@ -8,28 +8,42 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 import "erc721a/contracts/IERC721A.sol";
 import "Contracts/Modulo_MultiContract/ModuloRaffle721A.sol";
 
-contract ModuloDeployer is Ownable, ReentrancyGuard {
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+contract ModuloDeployer is Ownable, VRFConsumerBaseV2 {
 
 struct RaffleTracker {
 
+    IERC721A AddressInstance;
     ModuloRaffle721A RaffleAddress;
     uint256 GameNo;
-    address Winner;
     address PrizeNFTAddress;
     uint256 PrizeNFTID;
-    uint256 ChainlinkRequestID;
-    uint256 ChainlinkRandomNumber;
+    address Winner;
     bool    GameComplete;
 
 }
+/// Chainlink = start
+    VRFCoordinatorV2Interface COORDINATOR;
+    uint64 s_subscriptionId = 2624;
+    address vrfCoordinator = 0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed;
+    bytes32 keyHash = 	0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f;
+    uint32 callbackGasLimit = 2500000;
+    uint16 requestConfirmations = 10;
+    uint32   public numWords =  1;
+    uint256  public s_requestId;
+    uint256  public RandomNumber;
+    uint     public fakerandom = 115792089237316195423570985008687907853269984665;
+/////// Chainlink VRF - End
 
 /// Game Settings
-    uint     public Raffle_price        = 0.01 ether;
-    uint     public Raffle_maxPerFree   = 1; 
-    uint     public Raffle_maxPerWallet = 10; 
-    uint     public Raffle_totalFree    = 249; 
-    uint     public Raffle_maxSupply    = 999;
-    uint     public Raffle_BlockDelay   = 10000;
+    uint     public price_d        = 1 ether;
+    uint     public maxPerFree_d   = 1; 
+    uint     public maxPerWallet_d = 10; 
+    uint     public totalFree_d    = 250; 
+    uint     public maxSupply_d    = 250;
+    uint     public BlockDelay_d   = 10000;
 
 
 /// Reference to current game and past games
@@ -37,8 +51,10 @@ struct RaffleTracker {
     RaffleTracker public CurrentRaffle;
     mapping(uint256 => RaffleTracker) public RaffleTrackerMapper;
 
-
-
+    constructor()
+    VRFConsumerBaseV2(vrfCoordinator) {
+        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+    }
 
     function create_New_Raffle(
                 string calldata RaffleName_, 
@@ -48,7 +64,7 @@ struct RaffleTracker {
 
 
         require(CurrentRaffle.GameNo < 1 || 
-                CurrentRaffle.GameComplete == true, "Game in Progress - Cannot start a new one");
+                CurrentRaffle.GameComplete == true, "Game in Progress");
 
 
         IERC721A PrizeNFT = IERC721A(PrizeNFTAddress_);
@@ -70,12 +86,12 @@ struct RaffleTracker {
 
         /// create a new Raffle
          CurrentRaffle.RaffleAddress = new ModuloRaffle721A(
-            Raffle_maxSupply,
-            Raffle_totalFree,
-            Raffle_maxPerWallet,
-            Raffle_maxPerFree,
-            Raffle_price,
-            Raffle_BlockDelay,
+            maxSupply_d,
+            totalFree_d,
+            maxPerWallet_d,
+            maxPerFree_d,
+            price_d,
+            BlockDelay_d,
             RaffleName_,
             RaffleID_
         );
@@ -84,6 +100,7 @@ struct RaffleTracker {
         CurrentRaffle.GameNo          = currentGameNo;
         CurrentRaffle.PrizeNFTAddress = PrizeNFTAddress_;
         CurrentRaffle.PrizeNFTID      = PrizeNFTID_;
+        CurrentRaffle.AddressInstance = PrizeNFT;
 
     }
 
@@ -95,28 +112,56 @@ struct RaffleTracker {
                 uint _maxSupply,
                 uint _BlockDelay) external onlyOwner {
 
-        require(CurrentRaffle.GameComplete == true, "Game in Progress - Cannot changes settings");
+        require(CurrentRaffle.GameComplete == true, "Game in Progress");
 
-        Raffle_price        = _price;
-        Raffle_maxPerFree   = _maxPerFree;
-        Raffle_maxPerWallet = _maxPerWallet;
-        Raffle_totalFree    = _totalFree;
-        Raffle_maxSupply    = _maxSupply;
-        Raffle_BlockDelay   = _BlockDelay;
-
+        price_d        = _price;
+        maxPerFree_d   = _maxPerFree;
+        maxPerWallet_d = _maxPerWallet;
+        totalFree_d    = _totalFree;
+        maxSupply_d    = _maxSupply;
+        BlockDelay_d   = _BlockDelay;
 
     }
 
-    function withdraw_Funds() external onlyOwner nonReentrant {
+/// Chainlink - Start
+
+    function call_Chainlink()  external onlyOwner   {
+
+        s_requestId = COORDINATOR.requestRandomWords(
+        keyHash,
+        s_subscriptionId,
+        requestConfirmations,
+        callbackGasLimit,
+        numWords
+        );
+
+    }
+
+    function fulfillRandomWords(uint256, uint256[] memory randomWords) internal override {
+        RandomNumber = randomWords[0];
+        PickWinner();
+
+    }
+
+    function PickWinner()  private  {
+
+        uint256 WinningTokenID =  (RandomNumber % maxSupply_d) + 1;
+        CurrentRaffle.Winner = CurrentRaffle.AddressInstance.ownerOf(WinningTokenID);
+        require(CurrentRaffle.Winner != address(0), "Error getting Winner");
+        CurrentRaffle.AddressInstance.transferFrom(address(this), CurrentRaffle.Winner, CurrentRaffle.PrizeNFTID);
+        CurrentRaffle.GameComplete = true;
+
+    }
+
+
+
+    function withdraw_Funds() external onlyOwner {
         (bool success, ) = msg.sender.call{value: address(this).balance}("");
         require(success, "Transfer failed.");
     }
 
-    function withdraw_Prize_NFT() external onlyOwner nonReentrant {
-
-        /// Only used for emergencies 
-        IERC721A PrizeNFT = IERC721A(CurrentRaffle.PrizeNFTAddress);
-        PrizeNFT.transferFrom(address(this), msg.sender, CurrentRaffle.PrizeNFTID);
+    function withdraw_Prize_NFT() external onlyOwner {
+        CurrentRaffle.AddressInstance.transferFrom(address(this), msg.sender, CurrentRaffle.PrizeNFTID);
 
     }
 
